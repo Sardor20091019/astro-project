c8a8fb8a06b1e8dede909bf8232ddeeac63b3c85
Sardor20091019 <sardor091019@gmail.com>
2026-08-24 18:00:05 +0500

    fix: update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -108,6 +108,7 @@ export default function Navbar() {
               <NavLink href="/" active={pathname === "/"}>Gallery</NavLink> 
               <NavLink href="/photos" active={pathname === "/photos"}>Photos</NavLink>
               <NavLink href="/leaderboard" active={pathname === "/leaderboard"}>Leaderboard</NavLink>
+              <NavLink href="/explore" active={pathname === "/explore"}>Explore</NavLink>
               {user && (
                 <NavLink href="/creator/photos" active={pathname === "/creator/photos"}>Author Photos</NavLink>
               )}
